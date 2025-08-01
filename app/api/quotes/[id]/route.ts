250dcffd5d57595232da28f2c6ac7dc840f5f59e
Felipe Pérez Upegui <fperezupegui@gmail.com>
2025-08-01 15:55:44 -0500

    fix: resolve test files and add content type

--- app/api/quotes/[id]/route.ts
+++ app/api/quotes/[id]/route.ts
@@ -7,6 +7,7 @@ const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Content-Type': 'application/json'
 };
 
 export async function GET(
--- app/api/quotes/route.ts
+++ app/api/quotes/route.ts
@@ -6,13 +6,14 @@ const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Content-Type': 'application/json'
 };
 
 export async function GET() {
   await connectToDatabase();
   const quotes = await Quote.find().lean();
   const cleanedQuotes = quotes.map(({ author, quote }) => ({ author, quote }));
-  return new NextResponse(JSON.stringify({ cleanedQuotes}), {
+  return new NextResponse(JSON.stringify(cleanedQuotes), {
     status: 200,
     headers: {
       ...corsHeaders
